0a6d15aa574acc496b87bfc3cf0522bca55f8ef1
Ruairi O Brien <ruairi.obrien@mckesson.com>
2020-12-02 09:34:38 +0000

    read file

--- src/adventofcode2020/core.clj
+++ src/adventofcode2020/core.clj
@@ -1,7 +1,19 @@
 (ns adventofcode2020.core
   (:gen-class))
 
+(defn process-file-by-lines
+  "Process file reading it line-by-line"
+  ([file]
+   (process-file-by-lines file identity))
+  ([file process-fn]
+   (process-file-by-lines file process-fn println))
+  ([file process-fn output-fn]
+   (with-open [rdr (clojure.java.io/reader file)]
+     (doseq [line (line-seq rdr)]
+       (output-fn
+         (process-fn line))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (process-file-by-lines "resources/day1.txt"))
